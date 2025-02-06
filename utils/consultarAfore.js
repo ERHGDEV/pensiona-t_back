88bb7e002711e18bbba44c1bed80fd024fcf3373
ERHGDEV <erhgdev@gmail.com>
2025-02-06 12:08:32 -0600

    Se adapta flujo a planes de suscripción

--- utils/consultarAfore.js
+++ utils/consultarAfore.js
@@ -1,11 +1,31 @@
 const { generarEmailAleatorio } = require('./emailUtils')
 const { incrementUserCounter } = require('./incrementUserCounter')
-const axios = require('axios')
 const User = require('../models/user')
+const axios = require('axios')
+const config = require('./config')
 
 const consultarAfore = async (tipo, valor, userId) => {
     const emailAleatorio = generarEmailAleatorio()
-    const url = `https://api.esar.io/sartoken/externos/web/localizatuafore/afore/${emailAleatorio}/${tipo}/${valor}`
+    const url = `${config.URL_MASIVO}/${emailAleatorio}/${tipo}/${valor}`
+
+    const user = await User.findById(userId)
+    if (!user) {
+        throw new Error('Usuario no encontrado')
+    }
+
+    const hoy = new Date()
+
+    // Definir los límites según el tipo de suscripción
+    const limites = {
+        free: 1,
+        pro: 10,
+        unlimited: Infinity
+    }
+
+    // Verificar si el usuario ha alcanzado su límite diario
+    if (user.aforesConsultadasHoy >= limites[user.subscription]) {
+        throw new Error('Has alcanzado el límite de consultas diarias')
+    }
 
     const response = await axios.get(url, {
         headers: {
@@ -18,6 +38,9 @@ const consultarAfore = async (tipo, valor, userId) => {
 
     if (response.data.claveAfore !== null) {
         await incrementUserCounter(userId, 'aforesConsultadas')
+        user.aforesConsultadasHoy += 1
+        user.fechaUltimaConsulta = hoy
+        await user.save()
     }
 
     return response
